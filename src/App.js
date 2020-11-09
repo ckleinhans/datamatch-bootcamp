de7eb1ec8ff393849b024b2cf84e03aeb045b700
Caelan Kleinhans <55991831+ckleinhans@users.noreply.github.com>
2020-11-09 17:43:43 -0600

    First commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Testing!<br></br>
+          <a href="https://datamatch-bootcamp-782e9.web.app/">See my TicTacToe app!</a>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
